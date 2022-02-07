1c8b8277f8450348cf78a8f54c92be080172e9cb
Michael <attractadore02@gmail.com>
2022-02-07 18:36:04 +0300

    Remove timing from tests

--- TestBitonicSort.cpp
+++ TestBitonicSort.cpp
@@ -2,27 +2,21 @@
 
 #include <gtest/gtest.h>
 
-#include <chrono>
-
-TEST(TestBitonicSort, TestGenerated) {
+TEST(TestBitonicSort, TestGeneratedInt) {
     constexpr unsigned min_size = 1u << 1; 
-    constexpr unsigned max_size = 1u << 27; 
+    constexpr unsigned max_size = 1u << 20;
 
+    std::vector<int> data, expected;
+    data.reserve(max_size);
+    expected.reserve(max_size);
     for (unsigned long long i = min_size; i <= max_size; i *= 2) {
-        std::vector<int> data(i);
+        data.resize(i);
         for (auto& e: data) {
             e = rand();
         }
-        std::vector<int> expected = data;
-        auto t0 = std::chrono::steady_clock::now();
+        expected = data;
         bitonicSort(data.begin(), data.end());
-        auto t1 = std::chrono::steady_clock::now();
         std::sort(expected.begin(), expected.end());
-        auto t2 = std::chrono::steady_clock::now();
-        std::cout << std::fixed;
-        std::cout << data.size() << ", ";
-        std::cout << (t1 - t0).count() / 1e9 << ", ";
-        std::cout << (t2 - t1).count() / 1e9 << "\n";
         ASSERT_EQ(data, expected);
     }
 }
