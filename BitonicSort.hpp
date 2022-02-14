a2e238206829207bd57d4db6e9076f89afeea4ef
Michael <attractadore02@gmail.com>
2022-02-15 01:53:43 +0300

    Add local memory

--- BitonicSort.hpp
+++ BitonicSort.hpp
@@ -102,12 +102,22 @@ constexpr const char* getKernelExtensionStr() {
     return getKernelExtensionStrImpl<CLType<T>>();
 }
 
+struct Kernel {
+    cl_kernel slow = nullptr;
+    cl_kernel fast = nullptr;
+    cl_kernel start = nullptr;
+    size_t local_sz = 0;
+
+    explicit operator bool() const {
+        return slow and fast and start and local_sz;
+    }
+};
+
 class BSContext {
-private:
     cl_context m_ctx = nullptr;
     cl_device_id m_dev = nullptr;
     cl_command_queue m_q = nullptr;
-    std::unordered_map<const char*, cl_kernel> m_kers;
+    std::unordered_map<const char*, Kernel> m_kers;
 
 public:
     BSContext();
@@ -130,11 +140,11 @@ public:
     }
 
     template<typename T>
-    cl_kernel kernel();
+    const Kernel& kernel();
 
 private:
     template<typename T>
-    cl_kernel buildKernel();
+    Kernel buildKernel();
 };
 
 inline BSContext::BSContext() {
@@ -155,7 +165,8 @@ inline BSContext::BSContext() {
 inline BSContext::~BSContext() {
     clFinish(m_q);
     for (auto& [_, ker]: m_kers) {
-        clReleaseKernel(ker);
+        clReleaseKernel(ker.slow);
+        clReleaseKernel(ker.fast);
     }
     clReleaseCommandQueue(m_q);
     clReleaseContext(m_ctx);
@@ -191,7 +202,7 @@ bool BSTypeSupported(BSContext& ctx) {
 }
 
 template<typename T>
-cl_kernel BSContext::kernel() {
+const Kernel& BSContext::kernel() {
     auto& ker = m_kers[getKernelTypeStr<T>()];
     if (!ker) {
         ker = buildKernel<T>();
@@ -200,11 +211,127 @@ cl_kernel BSContext::kernel() {
 }
 
 template<typename T>
-cl_kernel BSContext::buildKernel() {
+auto minVStr() {
+    if constexpr (std::is_floating_point_v<T>) {
+        return "\"(-1.0 / 0.0)\"";
+    }
+    else {
+        return std::to_string(std::numeric_limits<T>::min());
+    }
+};
+
+template<typename T>
+auto maxVStr() {
+    if constexpr (std::is_floating_point_v<T>) {
+        return "\"(1.0 / 0.0)\"";
+    }
+    else {
+        return std::to_string(std::numeric_limits<T>::max());
+    }
+};
+
+template<typename T>
+Kernel BSContext::buildKernel() {
     auto src =
     std::string(getKernelExtensionStr<T>()) +
     R"(
-    __attribute__((reqd_work_group_size(1, 1, 1)))
+    #ifndef LOCAL_SIZE_X
+    #define LOCAL_SIZE_X 256
+    #endif
+    enum { local_size_x = LOCAL_SIZE_X };
+
+    void loadCache(
+        __global const KERNEL_TYPE* data, uint cnt,
+        __local KERNEL_TYPE* cache, KERNEL_TYPE pad_value
+    ) {
+        size_t i = 2 * get_global_id(0);
+        size_t j = 2 * get_local_id(0);
+        cache[j    ] = (i     < cnt) ? data[i    ]: pad_value;
+        cache[j + 1] = (i + 1 < cnt) ? data[i + 1]: pad_value;
+    }
+
+    void storeCache(
+        __global KERNEL_TYPE* data, uint cnt,
+        __local const KERNEL_TYPE* cache
+    ) {
+        size_t i = 2 * get_global_id(0);
+        size_t j = 2 * get_local_id(0);
+        if (i     < cnt) {
+            data[i    ] = cache[j    ];
+        }
+        if (i + 1 < cnt) {
+            data[i + 1] = cache[j + 1];
+        }
+    }
+
+    __attribute__((reqd_work_group_size(local_size_x, 1, 1)))
+    __kernel void bitonicSortLocal(
+       __global KERNEL_TYPE* data, uint cnt,
+       uint seq_cnt, uint subseq_cnt
+    ) {
+        __local KERNEL_TYPE cache[2 * local_size_x];
+        size_t j = get_global_id(0);
+        uint po2cnt = 1 << (32 - clz(cnt - 1));
+        uint mask = ((2 * j)) | (2 * seq_cnt - 1) | (~(po2cnt - 1));
+        bool b_ascending = !(popcount(mask) & 1);
+        KERNEL_TYPE pad_value = b_ascending ? MAX_VALUE: MIN_VALUE;
+        loadCache(data, cnt, cache, pad_value);
+        barrier(CLK_LOCAL_MEM_FENCE);
+
+        size_t i = get_local_id(0);
+        for (; subseq_cnt; subseq_cnt /= 2) {
+            size_t block_base = (i & ~(subseq_cnt - 1)) * 2;
+            size_t block_offt = i & (subseq_cnt - 1);
+            size_t sml_idx = block_base | block_offt;
+            size_t big_idx = sml_idx + subseq_cnt;
+            if (b_ascending == (cache[big_idx] < cache[sml_idx])) {
+                KERNEL_TYPE temp = cache[sml_idx];
+                cache[sml_idx] = cache[big_idx];
+                cache[big_idx] = temp;
+            }
+            barrier(CLK_LOCAL_MEM_FENCE);
+        }
+
+        storeCache(data, cnt, cache);
+    };
+
+    __attribute__((reqd_work_group_size(local_size_x, 1, 1)))
+    __kernel void bitonicSortFastStart(
+       __global KERNEL_TYPE* data, uint cnt
+    ) {
+        __local KERNEL_TYPE cache[2 * local_size_x];
+        size_t j = get_global_id(0);
+        uint po2cnt = 1 << (32 - clz(cnt - 1));
+        uint mask = ((2 * j)) | (2 * local_size_x - 1) | (~(po2cnt - 1));
+        bool b_ascending_pad = !(popcount(mask) & 1);
+        KERNEL_TYPE pad_value = b_ascending_pad ? MAX_VALUE: MIN_VALUE;
+        loadCache(data, cnt, cache, pad_value);
+        barrier(CLK_LOCAL_MEM_FENCE);
+
+        size_t i = get_local_id(0);
+        mask = ((2 * j)) | (~(po2cnt - 1));
+        bool b_ascending = !(popcount(mask) & 1);
+        for (uint seq_cnt = 1; seq_cnt <= local_size_x; seq_cnt *= 2) {
+            bool b_bit_already_set = mask & seq_cnt;
+            b_ascending = b_ascending == b_bit_already_set;
+            for (uint subseq_cnt = seq_cnt; subseq_cnt; subseq_cnt /= 2) {
+                size_t block_base = (i & ~(subseq_cnt - 1)) * 2;
+                size_t block_offt = i & (subseq_cnt - 1);
+                size_t sml_idx = block_base | block_offt;
+                size_t big_idx = sml_idx + subseq_cnt;
+                if (b_ascending == (cache[big_idx] < cache[sml_idx])) {
+                    KERNEL_TYPE temp = cache[sml_idx];
+                    cache[sml_idx] = cache[big_idx];
+                    cache[big_idx] = temp;
+                }
+                barrier(CLK_LOCAL_MEM_FENCE);
+            }
+        }
+
+        storeCache(data, cnt, cache);
+    };
+
+    __attribute__((reqd_work_group_size(local_size_x, 1, 1)))
     __kernel void bitonicSort(
        __global KERNEL_TYPE* data, uint cnt,
        uint seq_cnt, uint subseq_cnt
@@ -215,16 +342,24 @@ cl_kernel BSContext::buildKernel() {
         bool b_ascending = !(popcount(mask) & 1);
         size_t block_base = (i & ~(subseq_cnt - 1)) * 2;
         size_t block_offt = i & (subseq_cnt - 1);
-        size_t sml_idx = (block_base | block_offt);
+        size_t sml_idx = block_base | block_offt;
         size_t big_idx = sml_idx + subseq_cnt;
-        if (b_ascending == (data[big_idx] < data[sml_idx])) {
-            KERNEL_TYPE temp = data[sml_idx];
-            data[sml_idx] = data[big_idx];
-            data[big_idx] = temp;
+        bool b_sort = sml_idx < cnt && big_idx < cnt;
+        if (b_sort) {
+            if (b_ascending == (data[big_idx] < data[sml_idx])) {
+                KERNEL_TYPE temp = data[sml_idx];
+                data[sml_idx] = data[big_idx];
+                data[big_idx] = temp;
+            }
         }
     })";
     auto src_c_str = src.c_str();
-    auto build_options = std::string("-DKERNEL_TYPE=") + getKernelTypeStr<T>();
+
+    auto local_sz = 16u;
+    auto build_options = std::string(" -DKERNEL_TYPE=") + getKernelTypeStr<T>() +
+                                     " -DLOCAL_SIZE_X=" + std::to_string(local_sz) +
+                                     " -DMIN_VALUE="    + minVStr<T>() +
+                                     " -DMAX_VALUE="    + maxVStr<T>();
 
     auto prog = clCreateProgramWithSource(m_ctx, 1, &src_c_str, nullptr, nullptr);
     auto prog_err = clBuildProgram(prog, 1, &m_dev, build_options.c_str(), nullptr, nullptr);
@@ -235,33 +370,53 @@ cl_kernel BSContext::buildKernel() {
         clGetProgramBuildInfo(prog, m_dev, CL_PROGRAM_BUILD_LOG, log.size(), log.data(), nullptr);
         throw std::runtime_error{std::string("Failed to build program:\n") + log.data()};
     }
-
-    auto ker = clCreateKernel(prog, "bitonicSort", nullptr);
+    auto slow_ker = clCreateKernel(prog, "bitonicSort", nullptr);
+    auto fast_ker = clCreateKernel(prog, "bitonicSortLocal", nullptr);
+    auto start_ker = clCreateKernel(prog, "bitonicSortFastStart", nullptr);
 
     clReleaseProgram(prog);
 
-    return ker;
+    return {
+        .slow = slow_ker,
+        .fast = fast_ker,
+        .start = start_ker,
+        .local_sz = local_sz,
+    };
 }
 
 template<typename T>
-inline void BSRunKernel(BSContext& ctx, cl_mem buf, size_t cnt) {
+void BSRunKernel(BSContext& ctx, cl_mem buf, size_t cnt) {
     auto q = ctx.queue();
     auto ker = ctx.kernel<T>();
     auto po2cnt = std::bit_ceil(cnt);
     auto pad_cnt = po2cnt - cnt;
-    clSetKernelArg(ker, 0, sizeof(buf), &buf);
+    clSetKernelArg(ker.slow, 0, sizeof(buf), &buf);
+    clSetKernelArg(ker.fast, 0, sizeof(buf), &buf);
+    clSetKernelArg(ker.start, 0, sizeof(buf), &buf);
     cl_uint clcnt = cnt;
     assert(clcnt == cnt);
-    clSetKernelArg(ker, 1, sizeof(clcnt), &clcnt);
-    for (cl_uint seq_cnt = 1; seq_cnt < cnt; seq_cnt *= 2) {
-        for (cl_uint subseq_cnt = seq_cnt; subseq_cnt >= 1; subseq_cnt /= 2) {
-            clSetKernelArg(ker, 2, sizeof(seq_cnt), &seq_cnt);
-            clSetKernelArg(ker, 3, sizeof(subseq_cnt), &subseq_cnt);
+    clSetKernelArg(ker.slow, 1, sizeof(clcnt), &clcnt);
+    clSetKernelArg(ker.fast, 1, sizeof(clcnt), &clcnt);
+    clSetKernelArg(ker.start, 1, sizeof(clcnt), &clcnt);
+    {
+        size_t global_sz = (cnt / 2 / ker.local_sz + (cnt / 2 % ker.local_sz != 0)) * ker.local_sz;
+        clEnqueueNDRangeKernel(q, ker.start, 1, nullptr, &global_sz, nullptr, 0, nullptr, nullptr);
+    }
+    for (cl_uint seq_cnt = 2 * ker.local_sz; seq_cnt < cnt; seq_cnt *= 2) {
+        cl_uint subseq_cnt = seq_cnt;
+        for (; subseq_cnt > ker.local_sz; subseq_cnt /= 2) {
+            clSetKernelArg(ker.slow, 2, sizeof(seq_cnt), &seq_cnt);
+            clSetKernelArg(ker.slow, 3, sizeof(subseq_cnt), &subseq_cnt);
             auto rem = pad_cnt & (2 * subseq_cnt - 1);
             auto disable_cnt = std::min<size_t>(rem, subseq_cnt) + (pad_cnt - rem) / 2;
             size_t global_sz = po2cnt / 2 - disable_cnt;
-            clEnqueueNDRangeKernel(q, ker, 1, nullptr, &global_sz, nullptr, 0, nullptr, nullptr);
+            global_sz = (global_sz / ker.local_sz + (global_sz % ker.local_sz != 0)) * ker.local_sz;
+            clEnqueueNDRangeKernel(q, ker.slow, 1, nullptr, &global_sz, nullptr, 0, nullptr, nullptr);
         }
+        clSetKernelArg(ker.fast, 2, sizeof(seq_cnt), &seq_cnt);
+        clSetKernelArg(ker.fast, 3, sizeof(subseq_cnt), &subseq_cnt);
+        size_t global_sz = (cnt / 2 / ker.local_sz + (cnt / 2 % ker.local_sz != 0)) * ker.local_sz;
+        clEnqueueNDRangeKernel(q, ker.fast, 1, nullptr, &global_sz, nullptr, 0, nullptr, nullptr);
     }
 }
 
